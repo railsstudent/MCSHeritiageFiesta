2d11aec02e5150a4f6aef988052b7488f07af6bc
Connie Leung <cleung334@gmail.com>
2013-10-23 01:27:57 +0800

    able to load image

---
+++ src/com/blueskyconnie/bluestonecrystal/DetailActivity.java
@@ -0,0 +1,108 @@
+package com.blueskyconnie.bluestonecrystal;
+
+import java.lang.ref.WeakReference;
+import java.math.BigDecimal;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.blueskyconnie.bluestonecrystal.data.Product;
+import com.blueskyconnie.bluestonecrystal.helper.ImageDecodeHelper;
+
+public class DetailActivity extends Activity {
+
+	private static final int REQ_WIDTH = 320;
+	private static final int REQ_HEIGHT = 320;
+
+	private WeakReference<DownloadImageTask> asyncTaskWeakRef;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_detail);
+		
+		if (getIntent() != null)  {
+			Product product = (Product) getIntent().getSerializableExtra("currentProduct");
+			if (product != null) { 
+				// retrieve image
+				startNewAsyncTask(product);
+			}
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		finish();
+	}
+
+	private void startNewAsyncTask(Product product) {
+		DownloadImageTask asyncTask = new DownloadImageTask(this);
+	    this.asyncTaskWeakRef = new WeakReference<DownloadImageTask >(asyncTask);
+	    // image url
+	    asyncTaskWeakRef.get().execute(product.getImageUrl(), product.getName(), 
+	    		product.getDescription(), product.getPrice().toPlainString());
+	}
+
+	private class DownloadImageTask extends AsyncTask<String, Void, Product> {
+
+		private final ProgressDialog dialog = new ProgressDialog(DetailActivity.this); 
+		private WeakReference<DetailActivity> fragmentWeakRef;
+		
+		private DownloadImageTask (DetailActivity fragment) {
+            this.fragmentWeakRef = new WeakReference<DetailActivity>(fragment);
+        }
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			dialog.setMessage(DetailActivity.this.getString(R.string.dowloading));
+			dialog.show();
+		}
+
+		@Override
+		protected Product doInBackground(String... params) {
+
+			Product product = new Product();
+			product.setName(params[1]);
+			product.setDescription(params[2]);
+			product.setPrice(new BigDecimal(params[3]));
+			product.setImageUrl(params[0]);
+
+//			Bitmap bitmapProduct = ImageDecodeHelper.decodeSampledBitmapFromStream(params[0], REQ_WIDTH, REQ_HEIGHT);
+			Bitmap bitmapProduct = ImageDecodeHelper.decodeSampledBitmapFromByteArray(params[0], REQ_WIDTH, REQ_HEIGHT);
+			if (bitmapProduct != null) {
+				product.setImage(bitmapProduct);
+			} else {
+				product.setImage(null);
+			}
+			return product;
+		}
+
+		@Override
+		protected void onPostExecute(Product result) {
+			super.onPostExecute(result);
+			if (this.fragmentWeakRef.get() != null) {
+				TextView tvItemName = (TextView) fragmentWeakRef.get().findViewById(R.id.tvItemName);
+				tvItemName.setText(result.getName());
+				
+				TextView tvItemDesc = (TextView) fragmentWeakRef.get().findViewById(R.id.tvItemDescription);
+				tvItemDesc.setText(result.getDescription());
+				
+				TextView tvItemPrice = (TextView) fragmentWeakRef.get().findViewById(R.id.tvItemPrice);
+				tvItemPrice.setText(result.getPrice().toPlainString());
+				
+				if (result.getImage() != null) {
+					ImageView imgView = (ImageView) fragmentWeakRef.get().findViewById(R.id.imgItem);
+					imgView.setImageBitmap(result.getImage());
+				}
+				dialog.dismiss();
+			}
+		}
+	}
+}
---
+++ src/com/blueskyconnie/bluestonecrystal/helper/HttpClientHelper.java
@@ -0,0 +1,85 @@
+package com.blueskyconnie.bluestonecrystal.helper;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public final class HttpClientHelper {
+
+
+	public static InputStream retrieveImage(String strUrl) {
+		
+		if (strUrl == null || strUrl.length() == 0) {
+			return null;
+		}
+		
+		InputStream is = null;
+		BufferedInputStream bis = null;
+		 try{
+			 URL url = new URL(strUrl);
+			 URLConnection conn = url.openConnection();
+			 HttpURLConnection httpConn = (HttpURLConnection)conn;
+			  httpConn.setRequestMethod("GET");
+			  httpConn.connect();
+		 
+			  if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				  is = httpConn.getInputStream();
+				  if (is != null) {
+					  bis = new BufferedInputStream(is);
+					  return bis;
+				  }
+			  } 
+		 }
+		 catch (IOException ex){
+			 ex.printStackTrace();
+		 }
+		 return null;
+	}
+	
+//	public static String retrieveProductXml(String url) {
+//		
+//		HttpClient client = new DefaultHttpClient();
+//		HttpGet getMethod = new HttpGet(url);
+//		InputStream is = null;
+//		BufferedInputStream bis = null;
+//		String data = null;
+//		try {
+//		      // Execute the method.
+//			  HttpResponse res = client.execute(getMethod);
+//			  res.getEntity().get
+//			  
+//			  is = res.getEntity().getContent();
+//			  int contentSize = (int) res.getEntity().getContentLength();
+//			  bis = new BufferedInputStream(is, 512);
+//		         
+//		      //data = new byte[contentSize];
+//		      int bytesRead = 0;
+//		      int offset = 0;
+//		         
+//		      while (bytesRead != -1 && offset < contentSize) {
+//		          bytesRead = bis.read(data, offset, contentSize - offset);
+//		          offset += bytesRead;
+//		     }
+//		      
+//	    } catch (IOException e) {
+//	    	e.printStackTrace();
+//	    } finally {
+//	      // Release the connection.
+//	    	try {
+//		    	if (bis != null) {
+//		    		bis.close();
+//		    	}
+//		    	if (is != null) {
+//		    		is.close();
+//		    	}
+//	    	} catch (IOException ex) {
+//	    		ex.printStackTrace();
+//	    	}
+//	    }  
+//		return data;
+//	}
+	
+}
